b63fef0bc1a0862fba8c135adb2da1eb5d986b0a
cryptobeijing <101955570+cryptobeijing@users.noreply.github.com>
2024-11-29 10:00:41 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 import time
 
-while True:
-    print("Hello World")
-    time.sleep(60)
+def main():
+    while True:
+        print("Hello, World!")
+        time.sleep(60)  # Wait for 60 seconds
 
+if __name__ == "__main__":
+    main()
